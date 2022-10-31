b5d748192c82b91833998d9eb334aec4bf17b3b0
Eric1408 <alu0101335339@ull.edu.es>
2022-10-31 04:55:34 +0000

    aproximando al enunciado

--- final.sh
+++ final.sh
@@ -6,8 +6,10 @@ HELP="Este programa muestra informacion acerca de los tipos de sistemas, donde e
 
 system_info ()
 {
+  # al llamar a las funciones los parametros posicionales pierden su valor
+
   if [ "$#" -eq 0 ]; then # El simbolo '#' contiene el numero de elementos en la linea de comandos
-    df -ah | awk '{printf "%-14s %-28s %-7s\n", $1, $6, $3}' | tail -n+2 | sort -f 
+    df -ah | awk '{printf "%-14s %-28s %-7s\n", $1, $6, $3}' | tail -n+2 | sort -fu -k1,1 
     #for var in $NAME; do
     #  echo "linea $var"
     #done  
