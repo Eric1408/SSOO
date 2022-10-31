ee11842a34b9c4b652cc3d1d1e2685b388eaaa59
Eric1408 <alu0101335339@ull.edu.es>
2022-10-31 04:43:07 +0000

    falta quitar repeticiones

---
+++ final.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+HEADER=$(df -ah | awk '{printf "%-14s %-28s %-7s\n", $1, $6, $3}' | head -1)
+NAME=$(df -ah | awk '{print $1, $3}' | tail -n+2 | sort -f)
+HELP="Este programa muestra informacion acerca de los tipos de sistemas, donde estan montado y el espacio que ocupan"
+
+system_info ()
+{
+  if [ "$#" -eq 0 ]; then # El simbolo '#' contiene el numero de elementos en la linea de comandos
+    df -ah | awk '{printf "%-14s %-28s %-7s\n", $1, $6, $3}' | tail -n+2 | sort -f 
+    #for var in $NAME; do
+    #  echo "linea $var"
+    #done  
+  fi
+}
+
+case $1 in
+  "-h" | "--help") 
+    echo $HELP
+    ;;
+  "-inv" ) 
+    ## echo -e $HEADER por que al hacer la expansion no funciona el formateo del texto?
+    df -ah | awk '{printf "%-14s %-28s %-7s\n", $1, $6, $3}' | head -1
+    system_info | sort -rf
+    ;;
+  "" )
+    df -ah | awk '{printf "%-14s %-28s %-7s\n", $1, $6, $3}' | head -1
+    system_info
+    ;;
+  * ) 
+    echo "Parametro incorrecto"
+    exit 1
+    ;;
+esac
+
