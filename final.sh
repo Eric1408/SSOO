f38679fb22fea2b9ca094f524797d5e54f266803
Eric1408 <alu0101335339@ull.edu.es>
2022-11-07 12:27:28 +0000

    c hecho

--- final.sh
+++ final.sh
@@ -5,50 +5,48 @@ TEXT_ULINE=$(tput sgr 0 1)
 TEXT_GREEN=$(tput setaf 2)
 TEXT_RESET=$(tput sgr0)
 
-HEADER=$(df -ahT | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | head -1) 
-NAME=$(df -ah | awk '{print $1, $3}' | tail -n+2 | sort -f)
+HEADER=$(df -ahT | head -1 | awk '{printf "%-14s %-14s %-28s %-7s %-15s %-13s\n", $1, $2, $7, $4, "Tot. Devices", "Tot. Used"}')  
 HELP="Este programa muestra informacion acerca de los tipos de sistemas, donde estan montado y el espacio que ocupan"
-var1=$(ps -A -o etimes | sed 1d | head | awk '{ sum+=$1} END {print sum}')
 
-## df -ahT | sort -b -k2,2 -u --debug #Comando para obtener tipos de sistemas de archivo unicos 
-## df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | sort -f -k1,1
+TABLA=$(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | sort -f -k1,1)
 
 system_info ()
 {
-  # al llamar a las funciones los parametros posicionales pierden su valor
-  # pasar parametros con system_info $@
+  it=0
+  if [ "$#" -eq 0 ]; then # El simbolo '#' contiene el numero de elementos en la linea de comandos  
+    for type in $(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{print $2}')
+    do
+      total_dev=$(df -ahTt $type | tail -n+2 | wc -l)
+      total_sum=$(df -aTt $type | tail -n+2 | awk '{sum+=$4} END {print sum}')
+      ((it=it+1))
 
-  if [ "$#" -eq 0 ]; then # El simbolo '#' contiene el numero de elementos en la linea de comandos
-    df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | sort -f -k1,1
-    # df -ahT | awk '{print $1, $2, $7, $4}' | tail -n+2 | sort -fu -k1,1 
-    
+      aux=$(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | sort -f -k1,1 | sed -n "$it"p)
+      echo "$aux" "$(printf "%-15s %-13s\n" "$total_dev" "$total_sum")" 
+    done
   else 
-    df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | sort -rf -k1,1
+    for type in $(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{print $2}')
+    do
+      total_dev=$(df -ahTt $type | tail -n+2 | wc -l)
+      total_sum=$(df -aTt $type | tail -n+2 | awk '{sum+=$4} END {print sum}')
+      ((it=it+1))
+      
+      aux=$(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | sort -f -k1,1 | sed -n "$it"p)
+      echo "$("$aux" "$(printf "%-15s %-13s\n" "$total_dev" "$total_sum")" | sort -rf -k1,1)" 
+    done
   fi
-
 }
 
 case $1 in
   "-h" | "--help") 
-    echo $HELP
+    echo "$HELP"
     ;;
   "-inv" ) 
-    #echo  $HEADER  #por que al hacer la expansion no funciona el formateo del texto?
-    df -ahT | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | head -1
-    # system_info | sort -rf
+    echo "${TEXT_ULINE}$HEADER${TEXT_RESET}"  #por que al hacer la expansion no funciona el formateo del texto?
     system_info $@
-    #echo "====================================================================================="
-    #echo "$(($var1/3600)) horas"
-    
     ;;
   "" )
-    df -ahT | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | head -1
-    #df -ahT | awk '{print $1, $2, $7, $4}' | head -1 | column -t
-    
+    echo "$TEXT_ULINE$HEADER$TEXT_RESET"
     system_info $@
-    #echo "====================================================================================="
-    #echo "$(($var1/3600)) horas"
-    
     ;;
   * ) 
     echo "Parametro incorrecto" 1>&2
