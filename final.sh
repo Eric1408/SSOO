0ad189cbba17cdc97976be7aa47b75dfd47b2363
Eric1408 <alu0101335339@ull.edu.es>
2022-11-07 14:28:32 +0000

    nada

--- final.sh
+++ final.sh
@@ -13,7 +13,7 @@ TABLA=$(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{printf "%-14s %-14s %-28s
 system_info ()
 {
   it=0
-  if [ "$#" -eq 0 ]; then # El simbolo '#' contiene el numero de elementos en la linea de comandos  
+  #if [ "$#" -eq 0 ]; then # El simbolo '#' contiene el numero de elementos en la linea de comandos  
     for type in $(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{print $2}')
     do
       total_dev=$(df -ahTt $type | tail -n+2 | wc -l)
@@ -23,17 +23,7 @@ system_info ()
       aux=$(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | sort -f -k1,1 | sed -n "$it"p)
       echo "$aux" "$(printf "%-15s %-13s\n" "$total_dev" "$total_sum")" 
     done
-  else 
-    for type in $(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{print $2}')
-    do
-      total_dev=$(df -ahTt $type | tail -n+2 | wc -l)
-      total_sum=$(df -aTt $type | tail -n+2 | awk '{sum+=$4} END {print sum}')
-      ((it=it+1))
-      
-      aux=$(df -ahT | tail -n+2 | sort -b -k2,2 -u | awk '{printf "%-14s %-14s %-28s %-7s\n", $1, $2, $7, $4}' | sort -f -k1,1 | sed -n "$it"p)
-      echo "$("$aux" "$(printf "%-15s %-13s\n" "$total_dev" "$total_sum")" | sort -rf -k1,1)" 
-    done
-  fi
+  #fi
 }
 
 case $1 in
@@ -42,11 +32,11 @@ case $1 in
     ;;
   "-inv" ) 
     echo "${TEXT_ULINE}$HEADER${TEXT_RESET}"  #por que al hacer la expansion no funciona el formateo del texto?
-    system_info $@
+    system_info | sort -rf -k1,1
     ;;
   "" )
     echo "$TEXT_ULINE$HEADER$TEXT_RESET"
-    system_info $@
+    system_info #$@
     ;;
   * ) 
     echo "Parametro incorrecto" 1>&2
